b7dfc0600e54608683197d7f9c66c0740e67c7b6
Ranjan Kumar <ranjan_kumar@persistent.co.in>
2013-12-03 17:43:25 +0530

    puppet manifest command using puppet faces.

--- manifests/manifests/vm.pp
+++ manifests/manifests/vm.pp
@@ -2,12 +2,25 @@ define windowsazure::vm (
   $vm_name,
   $vm_user,
   $image,
-  $password,
   $location,
-  $vm_size = 'Small'
+  $homedir,
+  $azure_management_certificate,
+  $azure_subscription_id,
+  $vm_size = 'Small',
+  $puppet_master_ip = undef,
+  $private_key_file = undef,
+  $certificate_file = undef ,
+  $storage_account_name = undef,
+  $cloud_service_name = undef,
+  $password = undef
 ) {
 
     Exec { path => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'] }
+
+    $cmd = "puppet node_azure create --vm-user $vm_user \
+           --management-certificate $azure_management_certificate \
+           --azure-subscription-id $azure_subscription_id \
+           --image $image --vm-name $vm_name --location $location"
 
     if $vm_name == undef {
       fail('No vm_name specified for provisioning VM.')
@@ -21,13 +34,49 @@ define windowsazure::vm (
       fail('No image specified for provisioning VM.')
     }
 
-    if $password == undef {
-      fail('No SSH password specified for provisioning VM.')
-    }
-
     if $location == undef {
       fail('No location specified for provisioning VM.')
     }
+
+    if $homedir == undef {
+      fail('Specify home directory path.')
+    }
+
+    if $azure_management_certificate == undef {
+      fail('Specify azure management certificate path.')
+    }
+
+    if $azure_subscription_id == undef {
+      fail('Specify subscription id.')
+    }
+
+    if $azure_subscription_id != undef {
+      $pmi = "--puppet-master-ip $puppet_master_ip"
+    }
+
+    if $password != undef {
+      $passwd = "--password $password"
+    }
+
+    if $storage_account_name != undef {
+      $san = " --storage-account-name $storage_account_name"
+    }
+
+    if $certificate_file != undef {
+      $crtf = "--certificate-file $certificate_file"
+    }
+
+    if $private_key_file != undef {
+      $pkf = "--private-key-file $private_key_file"
+    }
+
+    if $cloud_service_name != undef {
+      $csn = "--cloud-service-name $cloud_service_name"
+    }
+
+  $storage_account_name = undef,
+
+    $command = "$cmd $pmi $passwd $san $crtf $pkf $csn"
 
     if !defined( Package['azure'] ) {
       package { 'azure':
@@ -36,42 +85,9 @@ define windowsazure::vm (
       }
     }
 
-    file { ['/tmp/windowsazure/','/tmp/windowsazure/vm/']:
-      ensure => "directory",
-    }~>
-
-    file {"azure_vm_provisioner-${title}.rb":
-      ensure  => file,
-      path    => "/tmp/windowsazure/vm/azure_vm_provisioner-${title}.rb",
-      content => template('windowsazure/azure_vm_provisioner.rb.erb'),
-      owner   => root,
-      group   => root,
-      mode    => '0644'
-    }~>
-
-    file {"azure_vm_bootstrap.rb":
-      ensure  => file,
-      path    => "/tmp/windowsazure/vm/azure_vm_bootstrap.rb",
-      content => template('windowsazure/azure_vm_bootstrap.rb.erb'),
-      owner   => root,
-      group   => root,
-      mode    => '0644'
-    }~>
-
-    file {"puppet-community.sh.erb":
-      ensure  => file,
-      path    => "/tmp/windowsazure/vm/puppet-community.sh",
-      content => template('windowsazure/puppet-community.sh.erb'),
-      owner   => root,
-      group   => root,
-      mode    => '0644'
-    }~>
-
     exec {"Provisioning VM ${title}":
-      command     => "ruby /tmp/windowsazure/vm/azure_vm_provisioner-${title}.rb",
-      require     => File["azure_vm_provisioner-${title}.rb"],
-      subscribe   => File["azure_vm_provisioner-${title}.rb"],
-      refreshonly => true,
-      logoutput   => true
+      command    => "/bin/bash -c \"export HOME=$homedir; $command\"",
+      logoutput  => true
     }
+
 }
