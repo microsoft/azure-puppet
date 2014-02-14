4891cbadadde9b88cac9d94cacea0f70803f1e7a
Ranjan Kumar <ranjan_kumar@persistent.co.in>
2014-02-14 16:24:51 +0530

    Adding cloud service manifest.

---
+++ manifests/cloudservice.pp
@@ -0,0 +1,61 @@
+class windowsazure::cloudservice (
+  $azure_management_certificate,
+  $azure_subscription_id,
+  $cloud_service_name,
+  $description = undef,
+  $label = undef,
+  $location = undef,
+  $affinity_group_name = undef
+) {
+
+    Exec { path => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'] }
+
+    $cmd = "puppet azure_cloudservice create --cloud-service-name ${cloud_service_name} --management-certificate ${azure_management_certificate} --azure-subscription-id ${azure_subscription_id}"
+
+    if $azure_management_certificate == undef {
+      fail('Specify azure management certificate path.')
+    }
+
+    if $azure_subscription_id == undef {
+      fail('Specify subscription id.')
+    }
+
+    if $cloud_service_name == undef {
+      fail('Cloud service name is not specified for creating cloud service.')
+    }
+
+    if $label != undef {
+      $lbl = "--label ${label}"
+    }
+
+    if $description != undef {
+      $desc = "--description ${description}"
+    }
+
+    if $affinity_group_name != undef {
+      $agn = "--affinity-group-name ${affinity_group_name}"
+    }
+
+    if $cloud_service_name != undef {
+      $csn = "--cloud-service-name ${cloud_service_name}"
+    }
+
+    if $location != undef {
+      $loc = "--location '${location}'"
+    }
+
+    $puppet_command = "${cmd} ${lbl} ${desc} ${csn} ${agn} ${loc}"
+
+    if !defined( Package['azure'] ) {
+      package { 'azure':
+        ensure   => '0.6.0',
+        provider => 'gem',
+      }
+    }
+
+    exec {"Cloud service ${title}":
+      command    => $puppet_command,
+      logoutput  => true
+    }
+
+}
